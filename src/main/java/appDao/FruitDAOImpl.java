83c8fc265603e0ab55c45945a22d2e8cd1a67021
alexKamo <sashakamor@gmail.com>
2024-10-19 12:04:43 -0500

    upd

---
+++ src/main/java/appDao/FruitDAOImpl.java
@@ -0,0 +1,9 @@
+package appDao;
+
+public class FruitDAOImpl implements FruitDAO{
+    @Override
+    public void addFruit() {
+        System.out.println("Hi from addFruit method");
+        System.out.println("Here will be the logic of adding fruit into db...");
+    }
+}

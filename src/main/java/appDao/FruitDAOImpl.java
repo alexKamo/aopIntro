d4e81dfaf38e11553ea163f8afe0f41d96cb2f44
alexKamo <sashakamor@gmail.com>
2024-10-19 12:15:05 -0500

    upd

--- src/main/java/appDao/FruitDAOImpl.java
+++ src/main/java/appDao/FruitDAOImpl.java
@@ -1,5 +1,8 @@
 package appDao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class FruitDAOImpl implements FruitDAO{
     @Override
     public void addFruit() {
--- src/main/java/com/aop/demo/AopintroApplication.java
+++ src/main/java/com/aop/demo/AopintroApplication.java
@@ -1,7 +1,10 @@
 package com.aop.demo;
 
+import appDao.FruitDAO;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopintroApplication {
@@ -10,4 +13,13 @@ public class AopintroApplication {
 		SpringApplication.run(AopintroApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner commandLineRunner(FruitDAO fruitDAO){
+		return runner -> runAddFruit(fruitDAO);
+	}
+
+	private void runAddFruit(FruitDAO fruitDAO) {
+		fruitDAO.addFruit();
+	}
 }

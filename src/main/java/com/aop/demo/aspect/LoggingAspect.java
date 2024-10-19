1526aecce0c0c86a0432c94ff775f89ae7dc11b1
alexKamo <sashakamor@gmail.com>
2024-10-19 12:38:52 -0500

    upd

--- src/main/java/com/aop/demo/aspect/LoggingAspect.java
+++ src/main/java/com/aop/demo/aspect/LoggingAspect.java
@@ -1,4 +1,15 @@
 package com.aop.demo.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    @Before("execution(public void addFruit())")
+    public void beforeAdding(){
+        System.out.println("Logic and logs that run before add method");
+    }
 }
